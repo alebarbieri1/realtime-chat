7b4a13bafd6387aad2cb2a11217db94eae49ab7d
Alexandre Barbieri <Alexandre.Barbieri@dieboldnixdorf.com>
2017-02-13 15:06:27 -0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+// Socket.io and express are sharing the same http server
+var io = require('socket.io')(server);
+
+var messages = [];
+
+var storeMessage = function(name, data){
+    messages.push({name: name, data: data});
+    if (messages.length > 30){
+        // Remove a primeira mensagem do array
+        messages.shift(); 
+    }
+}
+
+var usersActive = [];
+
+var storeUserActive = function(user){
+    usersActive.push(user);
+}
+
+var removeUser = function(user){
+    userIndex = usersActive.indexOf(user);
+    usersActive.splice(userIndex, 1);
+    console.log(usersActive);
+}
+
+
+io.on('connection', function(client){
+    console.log('Client connected...');
+    // Emit the messages event on the client (browser)
+    client.emit('messages', {msg: 'Conectado'});
+
+    client.on('messages', function(data){
+        var nickname = client.nickname;
+        // Broadcast message to all other clientes connected
+        client.broadcast.emit("messages", '<b>'+nickname+'</b>' + ': ' + data);
+        // Send the same message back to our client
+        client.emit('messages', '<b>'+nickname+'</b>' + ": " + data);
+        storeMessage(nickname, data);
+    });
+
+    client.on('join', function(name){
+        client.nickname = name;
+        console.log(name + ' joined the chat...');
+        messages.forEach(function(message){
+            client.emit('messages', '<b>'+message.name+'</b>' + ": " + message.data);
+        })
+        // Store the new user active on the list
+        storeUserActive(name);
+        // User joined receive the list of users active
+        usersActive.forEach(function(user){
+            client.emit('users active', user);
+        });
+        // All others users receive the name of the new user active
+        client.broadcast.emit('users active', name);
+    });
+
+    client.on('disconnect', function(){
+        console.log(client.nickname + ' disconnected from chat...');
+        // Remove from the list the user that disconnected from the chat
+        removeUser(client.nickname);
+        // All users active receive the name of the user that disconnected from the chat
+        client.broadcast.emit("user disconnected", client.nickname);
+    });
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8080);
